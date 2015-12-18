eacdc64df183d4c4bd9de800875dc2d5a0e88adf
Hansol-Lim <molmoty@gmail.com>
2015-12-18 18:46:37 +0900

    regular expression case select option added
    regular expression case select option added

--- js/convertTemplate.js
+++ js/convertTemplate.js
@@ -3,34 +3,50 @@ convertTemplate = {
 			frame : "",
 			variable : {},
 			head : "{",
-			tail : "}"
+			tail : "}",
+			case : "gi"
 		},
 		from : function(option, simpleFormat) {
 			var Response = null;
+			var caseSet = "g i m gi gm im ig mg mi gim gmi img igm mig mgi";
 			var ERROR = {
 				status : false,
 				message : {
 					undefined : "Undefined Error: ",
 					noObject : "Can't find option object, please insert your own option.",
 					noFrame : "Can't find 'template' in option, please insert your own template.",
-					noVar : "Can't find 'variable' in option, please insert your own variable."
+					noVar : "Can't find 'variable' in option, please insert your own variable.",
+					caseMiss : "Case not Correct. Combine with (g, i, m)"
 				}
 			};
 			try {
+				var template = convertTemplate.template;
 				if (simpleFormat != undefined) {
-					var simpleFormat = {
-						frame : option,
-						variable : simpleFormat
-					};
-					option = simpleFormat;
+					var temp = convertTemplate.template;
+					temp.frame = option;
+					temp.variable = simpleFormat;
+					option = temp;
 				}
 				if (option) {
 					if (option.frame != undefined) {
 						if (option.variable != undefined) {
-							convertTemplate.set.frame(option.frame);
-							convertTemplate.set.variable(option.variable);
-							convertTemplate.set.rule(option.head, option.tail);
-							Response = convertTemplate.get.result();
+							if (option.head == null)
+								option.head = template.head;
+							if (option.tail == null)
+								option.tail = template.tail;
+							if (option.case == null)
+								option.case = template.case;
+							
+							if (caseSet.indexOf(option.case) > 0)
+							{
+								convertTemplate.set.frame(option.frame);
+								convertTemplate.set.variable(option.variable);
+								convertTemplate.set.rule(option.head, option.tail);
+								Response = convertTemplate.get.result();
+							}
+							else {
+								Response = setError(Repsonse, ERROR.message.caseMiss);
+							}
 						} else {
 							Response = setError(Response, ERROR.message.noVar);
 						}
@@ -97,11 +113,10 @@ convertTemplate = {
 
 				for ( var idx in realVar) {
 					frame = frame
-							.replace(new RegExp("\\"+convertTemplate.template.head+realVar[idx]+"\\"+convertTemplate.template.tail,"g"), realData[idx])
+							.replace(new RegExp("\\"+convertTemplate.template.head+realVar[idx]+"\\"+convertTemplate.template.tail,convertTemplate.template.case), realData[idx])
 							.replace(/&lt;/gi, "<")
 							.replace(/&gt;/gi, ">");
 				}
-
 				return frame;
 			}
 		}
